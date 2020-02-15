f88649dc931f76c227056ad4dd9a48828e1e541a
Mohsin Khan <mkbusiness255@gmail.com>
2020-02-15 09:19:56 +0530

    initial commit

--- trial/settings.py
+++ trial/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
